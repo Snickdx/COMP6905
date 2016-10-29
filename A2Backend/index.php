44bb850261d7d6ac6842470d348f274dcd8a48dd
Nicholas <snickdx@gmail.com>
2016-10-29 13:05:36 -0400

    big update

--- A2Backend/index.php
+++ A2Backend/index.php
@@ -1,32 +1,78 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'vendor/autoload.php';
 
+
+
+const DEFAULT_URL = 'https://comp6950a2.firebaseio.com';
+const DEFAULT_TOKEN = 'l97gPC93474K9eKvK6dLaw9HRerFjYmuc4dzxS4U';
+const DEFAULT_PATH = '/';
+
+
+
 $app = new \Slim\Slim();
+
+$app->add(new \CorsSlim\CorsSlim());
+
+$app->config('debug', true);
+
+$corsOptions = array(
+    "origin" => "*",
+    "maxAge" => 1728000,
+    "allowCredentials" => False,
+    "allowMethods" => array("POST, GET"),
+    "allowHeaders" => array("X-PINGOTHER")
+);
+
+$cors = new \CorsSlim\CorsSlim($corsOptions);
 
 $app->get('/hello/:name', function ($name) {
     echo "Hello, " . $name;
 });
 
-$app->get('/getView', function () {
-    $view = [
-        [
-            "company" => "Hugh Jass Co.",
-            "time" => 1477670341
-        ],
-        [
-            "company" => "Harambe Ltd.",
-            "time" => 1477670341
-        ],
-        [
-            "company" => "Hugh Jass Co.",
-            "time" => 1477670341
-        ]
-    ];
+$app->get('/requests', function () {
 
+    $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
 
-    echo json_encode($view);
+    $data = $firebase->get('/requests');
+
+    echo json_encode($data);
 });
 
+$app->post('/sendrequest', function () {
+
+    $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+
+    $firebase->push("/requests/sent", [
+        "company" => $_POST['company'],
+        "time" => intval($_POST['time']),
+        "user" => $_POST['user']
+    ]);
+});
+
+$app->post('/deleterequest', function () {
+
+    $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+     $data = $firebase->get("requests/sent/".$_POST['key']);
+     $firebase->delete("requests/sent/".$_POST['key']);
+     $firebase->set("requests/deleted/".$_POST['key'], $data);
+
+
+});
+
+$app->post('/acceptrequest', function () {
+
+    $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+
+    $data = $firebase->get("requests/sent/".$_POST['key']);
+    $firebase->delete("requests/sent/".$_POST['key']);
+    $firebase->set("requests/accepted/".$_POST['key'], $data);
+});
 
 $app->run();
+
+?>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,103 @@
+(function(){
+    "use strict";
 
+    var app = angular.module('myApp', ['firebase', 'toastr']);
+
+    var HOST = "http://a2backend";
+
+    app.config(function(toastrConfig) {
+        angular.extend(toastrConfig, {
+            autoDismiss: false,
+            containerId: 'toast-container',
+            maxOpened: 0,
+            newestOnTop: true,
+            timeout: 3000,
+            positionClass: "toast-bottom-full-width",
+            preventDuplicates: false,
+            preventOpenDuplicates: false,
+            target: 'body'
+        });
+    });
+
+    app.controller("mainController", ["$http", "$scope", "$firebaseArray", "toastr", function ($http, $scope, $firebaseArray, toastr) {
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyDmuJBICvJPCpq3D0FPsan7IfzjAXhmz5c",
+            authDomain: "comp6950a2.firebaseapp.com",
+            databaseURL: "https://comp6950a2.firebaseio.com",
+            storageBucket: "comp6950a2.appspot.com",
+            messagingSenderId: "686237011839"
+        };
+        firebase.initializeApp(config);
+
+        var db = firebase.database();
+
+        $scope.input = {
+            user : "John",
+            time : moment(),
+            company: "Harambe Ltd."
+        };
+
+        /**
+         * View Retrieves all pending requests from all users
+         *
+             | User | Company      | Timestamp |
+             |------|--------------|-----------|
+             | John | Harambe Ltd  | 128123243 |
+             | Bill | Fotomart Inc | 213434234 |
+         */
+        $scope.data = $firebaseArray(db.ref("/requests/sent"));
+
+        $scope.print = function(timestamp){
+            return moment(timestamp).format("MMM Do YY hh:mm:ss a");
+        };
+
+        $scope.now = function(){
+            $scope.input.time = moment();
+        };
+
+        $scope.unix = function(date){
+            return moment(date).unix();
+        };
+
+        $scope.cancel = function(item){
+            // console.log(item);
+            $http({
+                method: 'POST',
+                url: HOST+"/deleterequest",
+                data: "key="+item.$id,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+            })
+                .success(function(data, status){
+                    console.log(data);
+                    console.log(status);
+                    toastr.success('Success', 'Request Cancelled!');
+                })
+                .error(function(data, status){
+                    console.log(data);
+                    console.log(status);
+                });
+        };
+
+        $scope.send = function(){
+            console.log($scope.input);
+            $http({
+                method: 'POST',
+                url: HOST+"/sendrequest",
+                data: "user="+$scope.input.user+"&company="+$scope.input.company+"&time="+moment($scope.input.time).unix(),
+                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+            })
+                .success(function(data, status){
+                    console.log(data);
+                    console.log(status);
+                    toastr.success('Success', 'Request Sent!');
+                })
+                .error(function(data, status){
+                    console.log(data);
+                    console.log(status);
+                });
+        };
+
+
+    }]);
+})();
